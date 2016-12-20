6f34ef9a641c6165ae0d7a7f930416457542e865
abbas english <abbas.shahab84@yahoo.com>
2016-12-20 16:39:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.size == 2
+      name = array.pop
+      array.push(" and #{name}")
+      array.join
+    elsif array.size > 2
+      name = array.pop
+      array.push("and #{name}")
+      array.join(", ")
+    elsif array.size == 1
+    array.join
+    end
 end
+array = ["kiwi"]
+puts oxford_comma(array)
