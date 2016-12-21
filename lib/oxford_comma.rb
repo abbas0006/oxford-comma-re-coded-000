efcba0fc28d37dd3613555f2efb85aceeb462c03
abbas english <abbas.shahab84@yahoo.com>
2016-12-21 07:34:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,24 @@
 def oxford_comma(array)
-    if array.size == 2
-      name = array.pop
-      array.push(" and #{name}")
-      array.join
-    elsif array.size > 2
-      name = array.pop
-      array.push("and #{name}")
-      array.join(", ")
-    elsif array.size == 1
-    array.join
-    end
-end
-array = ["kiwi"]
-puts oxford_comma(array)
+    if array.size == 1
+     array[0]
+   elsif array.size==2
+     array.join(" and ")
+ else
+   last=array.pop
+   array.join(", ")<<", and "<<last
+ end
+
+ end
+
+      # array.push(" and #{name}")
+#       array.join
+#     elsif array.size > 2
+#       name = array.pop
+#       array.push("and #{name}")
+#       array.join(", ")
+#     elsif array.size == 1
+#     array.join
+#     end
+# end
+# array = ["kiwi"]
+# puts oxford_comma(array)
